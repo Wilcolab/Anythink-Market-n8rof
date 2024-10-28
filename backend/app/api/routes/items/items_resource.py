989ae0bda5e2dd463e6626ef65516c4c6dcdbec8
razramon <razramon123@gmail.com>
2024-10-28 17:15:08 +0000

    first testing for dall-e feature

--- backend/app/api/routes/items/items_resource.py
+++ backend/app/api/routes/items/items_resource.py
@@ -22,7 +22,7 @@ from app.models.schemas.items import (
     ListOfItemsInResponse,
 )
 from app.resources import strings
-from app.services.items import check_item_exists, get_slug_for_item
+from app.services.items import check_item_exists, get_slug_for_item, get_image_for_item
 from app.services.event import send_event
 
 router = APIRouter()
@@ -68,6 +68,12 @@ async def create_new_item(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=strings.ITEM_ALREADY_EXISTS,
         )
+    if(item_create.image == None):
+        item_create.image = await get_image_for_item(item_create.title):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=strings.ITEM_ALREADY_EXISTS,
+            )
     item = await items_repo.create_item(
         slug=slug,
         title=item_create.title,
--- backend/app/services/items.py
+++ backend/app/services/items.py
@@ -4,7 +4,7 @@ from app.db.errors import EntityDoesNotExist
 from app.db.repositories.items import ItemsRepository
 from app.models.domain.items import Item
 from app.models.domain.users import User
-
+from openai import OpenAI, OpenAIError
 
 async def check_item_exists(items_repo: ItemsRepository, slug: str) -> bool:
     try:
@@ -21,3 +21,20 @@ def get_slug_for_item(title: str) -> str:
 
 def check_user_can_modify_item(item: Item, user: User) -> bool:
     return item.seller.username == user.username
+
+async def get_image_for_item(title: str) -> str:
+
+    client = OpenAI()
+    try:
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=title,
+            size="256x256",
+            quality="standard",
+            n=1,
+        )
+    except OpenAIError as e:
+        print(e.http_status)
+        print(e.error)
+    print(response.data[0].url)
+    return response.data[0].url
