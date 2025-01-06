b1226f36cb814b139456c31788d4810cea1bdc95
razramon <razramon123@gmail.com>
2025-01-06 11:04:47 +0000

    added dall-e 2 request

--- backend/app/api/routes/items/items_resource.py
+++ backend/app/api/routes/items/items_resource.py
@@ -68,12 +68,10 @@ async def create_new_item(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=strings.ITEM_ALREADY_EXISTS,
         )
+        
     if(item_create.image == None):
-        item_create.image = await get_image_for_item(item_create.title):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=strings.ITEM_ALREADY_EXISTS,
-            )
+        item_create.image = await get_image_for_item(item_create.title)
+
     item = await items_repo.create_item(
         slug=slug,
         title=item_create.title,
--- backend/app/services/items.py
+++ backend/app/services/items.py
@@ -4,7 +4,8 @@ from app.db.errors import EntityDoesNotExist
 from app.db.repositories.items import ItemsRepository
 from app.models.domain.items import Item
 from app.models.domain.users import User
-from openai import OpenAI, OpenAIError
+import requests
+import os
 
 async def check_item_exists(items_repo: ItemsRepository, slug: str) -> bool:
     try:
@@ -23,18 +24,19 @@ def check_user_can_modify_item(item: Item, user: User) -> bool:
     return item.seller.username == user.username
 
 async def get_image_for_item(title: str) -> str:
+    api_key = os.getenv("OPENAI_API_KEY")
+    url = "https://api.openai.com/v1/images/generations"
+    payload = {
+        "model":"dall-e-2",
+        "prompt": title,
+        "n": 1,
+        "quality":"standard",
+        "size": "256x256"
+    }
 
-    client = OpenAI()
-    try:
-        response = client.images.generate(
-            model="dall-e-3",
-            prompt=title,
-            size="256x256",
-            quality="standard",
-            n=1,
-        )
-    except OpenAIError as e:
-        print(e.http_status)
-        print(e.error)
-    print(response.data[0].url)
-    return response.data[0].url
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    return response.data
